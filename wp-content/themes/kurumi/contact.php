66b63f96d5a2397094a01bbdae6bff21d2066a11
dophu17 <dophu17@gmail.com>
2016-09-12 10:37:03 +0700

    update contact form 7

--- wp-content/themes/kurumi/contact.php
+++ wp-content/themes/kurumi/contact.php
@@ -12,8 +12,9 @@
 			<p>Ghi địa chỉ vào đây</p>
 			<p>090 456 765</p>
 		</div>
+		<br>
 		<div class="contact-form">
-		        <?php echo do_shortcode('[CONTACT FORM]'); ?>
+		        <?php echo do_shortcode('[contact-form-7 id="15" title="Contact form 1"]'); ?>
 		</div>
 
 	</div><!-- .container -->
